d8e26b2e074442c566f8959cb399254448124e7c
WhiteRabbit2312 <59802491+WhiteRabbit2312@users.noreply.github.com>
2024-05-30 17:48:09 +0300

    Update input weapon

--- Assets/MyProject/Scripts/LeftHandShooting.cs
+++ Assets/MyProject/Scripts/LeftHandShooting.cs
@@ -5,15 +5,5 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class LeftHandShooting : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
--- Assets/MyProject/Scripts/Weapon/Shooting.cs
+++ Assets/MyProject/Scripts/Weapon/Shooting.cs
@@ -5,10 +5,24 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class Shooting : NetworkBehaviour
 {
-    [SerializeField] private InputActionReference _actionReferenceShootingUnityEditor;
-    [SerializeField] private InputActionReference _actionReferenceShooting;
+    [SerializeField] private InputActionReference _actionReferenceShootingUnityEditorRight;
+    [SerializeField] private InputActionReference _actionReferenceShootingUnityEditorLeft;
+    [SerializeField] private InputActionReference _actionReferenceShootingLeft;
+    [SerializeField] private InputActionReference _actionReferenceShootingLeftRight;
     [SerializeField] private InputActionReference _actionRemoveAmmoLeft;
     [SerializeField] private InputActionReference _actionRemoveAmmoRight;
+    [SerializeField] private InputActionReference _actionRGrabLeft;
+    [SerializeField] private InputActionReference _actionRGrabRight;
+
+    [Space]
+    [SerializeField] private InputActionReference _testShootLeft;
+    [SerializeField] private InputActionReference _testShootRight;
+    [SerializeField] private InputActionReference _testGrabLeft;
+    [SerializeField] private InputActionReference _testGrabRight;
+    [SerializeField] private InputActionReference _testRemoveLeft;
+    [SerializeField] private InputActionReference _testRemoveRight;
+
+    [Space]
 
     [SerializeField] private InputActionReference _actionNButton;
 
@@ -19,31 +33,59 @@ public class Shooting : NetworkBehaviour
 
     [SerializeField] private Ammunition _ammunition;
     private PlayerStats _playerStats;
-    private InputActionReference _myAction;
+    private InputActionReference _myActionRemoveAmmo;
+    private InputActionReference _myActionShoot;
+
+
     private Rigidbody _ammoBoxRB;
     private XRGrabInteractable _grabInteractable;
 
     private XRController _xRController;
     private Vector3 _gunScale = new Vector3(1f, 1f, 1f);
+    private bool _allowToGrab = true;
 
     public override void Spawned()
     {
         _playerStats = GetComponentInParent<PlayerStats>();
         _grabInteractable = GetComponent<XRGrabInteractable>();
-        _myAction = _actionReferenceShootingUnityEditor;
-        _myAction.action.started += ShootInput;
+        
+        
     }
 
     public override void FixedUpdateNetwork()
     {
+        if (_grabInteractable.isSelected)
+        {
+            Debug.LogError("Grab");
+            if (_actionRGrabLeft.action.IsPressed())
+            {
+                Debug.LogError("Left grab");
+                _myActionShoot = _actionReferenceShootingLeft;
+                _myActionRemoveAmmo = _actionRemoveAmmoLeft;
+                _myActionShoot.action.started += ShootInput;
+            }
+
+            if (_actionRGrabRight.action.IsPressed())
+            {
+                Debug.LogError("Right grab");
+                _myActionShoot = _actionReferenceShootingLeftRight;
+                _myActionRemoveAmmo = _actionRemoveAmmoRight;
+                _myActionShoot.action.started += ShootInput;
+
+            }
+
+        }
+
+
         if (!_grabInteractable.isSelected)
         {
+            _myActionShoot.action.started -= ShootInput;
             transform.position = _gunPlace.transform.position;
             transform.rotation = _gunPlace.transform.rotation;
             transform.localScale = _gunScale;
         }
 
-        if (_actionRemoveAmmoRight.action.IsPressed() || _actionNButton.action.IsPressed())
+        if (_myActionRemoveAmmo.action.IsPressed() || _actionNButton.action.IsPressed())
         {
             _ammoBoxRB.isKinematic = false;
             _ammoBoxRB.gameObject.transform.SetParent(null);
@@ -59,6 +101,7 @@ public class Shooting : NetworkBehaviour
 
     public void ShootInput(InputAction.CallbackContext context)
     {
+        Debug.LogError("Shoot");
         if (HasInputAuthority && _grabInteractable.isSelected && _ammunition.AmmoSetToGun)
         {
             if(_playerStats.Ammo > 0)
